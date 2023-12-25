af3903b8f01f2ef23ca946e74afd475c8b49dc55
Ken C <kenwillcode@outlook.com>
2023-12-25 12:46:25 -0500

    new origin first commit

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,12 +1,12 @@
 // components/Index.js
 import React from 'react';
 import '../styles/Index.css';
-
+import kenSuit from '../resources/Ken_Suit.jpg'
 const Index = () => {
   return (
     <center><div id="index">
       <h2>Welcome to My Portfolio</h2>
-      <img src='Ken_Suit.jpg' id="port-pic" alt="Portfolio of Ken Crawford"></img>
+      <img src={kenSuit} id="port-pic" alt="Portfolio of Ken Crawford"></img>
       {/* Add your content here */}
     </div></center>
   );
